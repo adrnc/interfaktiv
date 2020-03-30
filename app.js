66103118511ae4f31d0d705023f76b3e7a060c11
adrianonym <49120352+adrianonym@users.noreply.github.com>
2020-03-30 13:15:44 +0200

    quiz

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ async function loadTopic(event) {
 	sources = querySelector('.js-sources');
 
 	while (introduction.firstChild) introduction.removeChild(introduction.firstChild);
+	while (activity.firstChild) activity.removeChild(activity.firstChild);
+	while (text.firstChild) text.removeChild(text.firstChild);
+	while (quiz.firstChild) quiz.removeChild(quiz.firstChild);
+	while (results.firstChild) results.removeChild(results.firstChild);
+	while (sources.firstChild) sources.removeChild(sources.firstChild);
+
 	topicData.introduction.forEach(item => {
 		const element = createElement(item[0]);
 		if (content.includes(item[0])) element.innerHTML = item[1].replace(/>/gm, '</strong>').replace(/<(?!\/strong>)/gm, '<strong>');
@@ -61,9 +67,6 @@ async function loadTopic(event) {
 	});
 	buttonMap(contentList);
 	introduction.appendChild(contentList);
-
-	while (activity.firstChild) activity.removeChild(activity.firstChild);
-	while (text.firstChild) text.removeChild(text.firstChild);
 
 	let subnumber = 1;
 	topicData.activity.forEach(item => {
@@ -124,12 +127,86 @@ async function loadTopic(event) {
 	activity.appendChild(lastActivity);
 	buttonMap(activity);
 
+	subnumber = 1;
+	topicData.quiz.forEach(item => {
+		const subActivity = createElement('div'),
+		question = createElement('h2'),
+		questionList = createElement('p'),
+		answer = createElement('h2'),
+		answerList = createElement('p');
+
+		addClass(questionList, 'answerwidth');
+		addClass(answerList, 'answerwidth');
+
+		subActivity.dataset.subpage = subnumber++;
+		addClass(subActivity, 'activity-animation');
+		question.appendChild(createTextNode(item[0]));
+		answer.appendChild(createTextNode(item[0]));
+
+		const continueButton = createElement('button');
+		continueButton.appendChild(createTextNode('Weiter'));
+		continueButton.className = 'js-continue continuebutton invisiblebutton';
+		continueButton.dataset.anchor = topicData.quiz.length + 1 == subnumber ? 'ergebnisse' : `quiz-${subnumber}`;
+		continueButton.disabled = true;
+		
+		let choice = 1;
+		item.slice(1).forEach(response => {
+			const button = createElement('button'),
+			answerButton = createElement('button');
+
+			button.dataset.choice = choice;
+			button.dataset.correct = response[1];
+			answerButton.dataset.choice = choice++;
+
+			button.appendChild(createTextNode(response[0]));
+			answerButton.appendChild(createTextNode(response[0]));
+			questionList.appendChild(button);
+			answerList.appendChild(answerButton);
+
+			addClass(answerButton, response[1] ? 'correctanswer' : 'wronganswer');
+
+			button.addEventListener('click', event => {
+				event.currentTarget.parentElement.querySelectorAll('.selectedbutton').forEach(element => removeClass(element, 'selectedbutton'));
+				continueButton.disabled = false;
+				removeClass(continueButton, 'invisiblebutton');
+				addClass(event.currentTarget, 'selectedbutton');
+
+				answerList.querySelectorAll('.correctchoice, .wrongchoice').forEach(element => {
+					removeClass(element, 'correctchoice');
+					removeClass(element, 'wrongchoice');
+				});
+
+				addClass(answerList.querySelector(`[data-choice="${event.currentTarget.dataset.choice}"]`), event.currentTarget.dataset.correct == "true" ? 'correctchoice' : 'wrongchoice');
+			}, false);
+		});
+
+		subActivity.appendChild(question);
+		subActivity.appendChild(questionList);
+		subActivity.appendChild(continueButton);
+		results.appendChild(answer);
+		results.appendChild(answerList);
+
+		quiz.appendChild(subActivity);
+	});
+	buttonMap(quiz);
+
+	const	quizBack = createElement('button');
+	quizBack.dataset.anchor = 'einfuehrung';
+	quizBack.appendChild(createTextNode('Zurück zum Start'));
+	quizBack.style.width = 'auto';
+	results.appendChild(quizBack);
+	buttonMap(results);
+
 	topicData.sources.forEach(item => {
 		const p = createElement('p'),
 		a = createElement('a');
 
 		a.appendChild(createTextNode(item[0]));
-		if (item[1]) a.href = item[1];
+		if (item[1]) {
+			a.href = item[1];
+			a.target = '_blank';
+			a.rel = 'noopener norefferer';
+		}
 
 		p.appendChild(a);
 		sources.appendChild(p);
